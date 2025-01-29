d65c56542e35a0b4b36611b33503e326d60d1101
Alessandro Nocentini <alessandro.nocentini@mail.polimi.it>
2025-01-29 11:11:34 +0100

    Uploading all the portfolio page files

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,14 @@
+<?php
+
+    if(!empty($_POST["message"])) {
+
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        $message = $_POST["message"];
+        $receiving_email_address = $_POST["alessandro.nocentini@mail.polimi.it"];
+
+
+    }
+
+?>
